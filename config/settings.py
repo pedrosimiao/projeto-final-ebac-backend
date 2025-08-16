b0a0d4961a4fd1d4f8bab48e6ed01ab8854d01bd
pedrosimiao <pedrolumiere@gmail.com>
2025-08-16 04:04:41 -0300

    feat: config images storage AWS S3

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,19 @@ CORS_ALLOWED_ORIGINS = env.list(
 )
 CORS_ALLOW_CREDENTIALS = True
 
+
+# AWS S3
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+
+
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -75,6 +88,7 @@ INSTALLED_APPS = [
     "follows",
     "notifications",
     "hashtags",
+    "storages",
 ]
 
 if DEBUG:
